054dc86a7115a8c66ceba48036df0d13b2149081
yurleb <ylebedev@borderstates.com>
2021-10-25 20:56:45 -0400

    init

---
+++ mlp.py
@@ -0,0 +1,23 @@
+from torch import nn
+import torch.nn.functional as F
+
+
+class MLP(nn.Module):
+    """
+    Linear (256) -> ReLU -> Linear(64) -> ReLU -> Linear(10) -> ReLU-> LogSoftmax
+    """
+
+    def __init__(self, l1=256, l2=64):
+        super().__init__()
+        self.fc1 = nn.Linear(784, l1)
+        self.fc2 = nn.Linear(l1, l2)
+        self.fc3 = nn.Linear(l2, 10)
+
+    def forward(self, x):
+        x = x.view(x.shape[0], -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.log_softmax(x, dim=1)
+
+        return x
